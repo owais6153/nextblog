12a177cc77e6640286c2b22243b42c51bc9f98eb
NadirBakhsh <nadir4k2010@gmail.com>
2024-01-20 02:43:16 +0500

    layout building

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,43 @@
 import Image from "next/image";
-import { AspectRatio } from "@/components/ui/aspect-ratio"
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 export default function Home() {
   return (
     <main className="relative">
+      <div className="w-full absolute -z-[1]">
+        <Image
+          width={1920}
+          height={14687}
+          src="/images/themlayout.png"
+          alt="Image"
+          className="object-contain"
+        />
+      </div>
 
-    <div className="w-full absolute -z-[1]">
-        <Image width={1920} height={14687}  src="/images/themlayout.png" alt="Image" className="object-contain" />
-    </div>
+      {/* Header Start */}
+      <header className="w-full h-[180px] bg-black"></header>
+      {/* Header close */}
 
-    <div className="w-full h-[180px] bg-slate-800 opacity-50">
+      {/* Nav Bar start */}
+      <nav className="w-full bg-slate-400 h-[44px]"></nav>
+      {/* Nav Bar close */}
 
-    </div>
+      {/* Google Add space start */}
+      <div className="w-full h-[280px] bg-black opacity-30"></div>
+      {/* Google Add space close */}
 
+      {/* Hero Section Layout start */}
+      <div className="w-full h-[1280px] bg-green-600 opacity-60 flex flex-wrap ">
+        {/* Trending section */}
+        <section className="bg-red-600  flex-1 h-full shrink-0 "></section>
+
+        {/* Live or main section */}
+        <section className="bg-gray-800 flex-1 h-full shrink-0 "></section>
+
+        {/* Latest section */}
+        <section className="bg-black-500 flex-1 h-full shrink-0 "></section>
+      </div>
+      {/* Hero Section Layout close */}
     </main>
   );
 }
